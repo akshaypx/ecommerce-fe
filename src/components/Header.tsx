09c6dceeaae365517ca7adbf276a5335e6ba3e88
akshaypx <akshaysrivastava46@gmail.com>
2024-11-16 23:00:55 +0530

    fix:Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const Header = () => {
-  const navigate = useNavigate();
   const { user, logout } = useAuth();
 
   const handleLogout = async () => {
